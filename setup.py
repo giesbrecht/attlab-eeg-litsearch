448060dfa2211c9af94e2974c8182db955352f7f
Jordan Garrett <jordangarrett@ucsb.edu>
2022-08-26 12:10:36 -0700

    scrape scripts

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BarryScrape',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Jordan',
+    author_email='jordangarrett@ucsb.edu',
+    description='Scraping Proquest Results From Barry\'s EEG Search'
+)
